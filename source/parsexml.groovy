5c45bf47f553428b6bf7edcf78b1e75b8cff1998
Hector Garcia Rosales <ghectormus@gmail.com>
2022-09-06 12:00:56 -0500

    Adding a readaer for specific position element

--- source/parsexml.groovy
+++ source/parsexml.groovy
@@ -15,3 +15,4 @@ println guitars.getClass().getName()*/
 def guitars = new XmlSlurper().parse('data/guitars.xml') // this will readfrom a file with parsemethdd
 
 println guitars //this prints like this cause is the node
+println guitars.guitar[2].name // reading from traversing the xml , reding the number 3 aelemernt on file
