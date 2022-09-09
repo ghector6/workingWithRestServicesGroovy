4633f872bc542de97df874bc71bbc4c25c034c3a
Hector Garcia Rosales <ghectormus@gmail.com>
2022-09-08 21:51:05 -0500

    Adding reader

--- source/norrisDV.groovy
+++ source/norrisDV.groovy
@@ -1,3 +1,1 @@
-@Grab('com.github.groovy-wslite:groovy-wslite:1.1.3') //adding external dependency to have a RESTClient
-
-String base = 'https://api.icndb.com'
+println 'https://groovy-lang.org/'.toURL().text
